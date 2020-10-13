01a657473c6e1d8fecc5e085fa4b5045ad964f50
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-13 05:40:40 +0300

    Add method to save, delete and find user

--- password.py
+++ password.py
@@ -5,15 +5,40 @@ class User:
     """
 
     user_list = []
-    def __init__(self,name,username,password):
-        self.name = name
+    def __init__(self,first_name,second_name,username,password):
+        self.first_name = first_name
+        self.second_name = second_name
         self.username = username
         self.password = password
 
     def save_user(self):
 
-        '''
+        """
         save_user method saves user objects into user_list
-        '''
+        """
 
-        User.user_list.append(self)    
+        User.user_list.append(self) 
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+        User.user_list.remove(self)    
+
+    @classmethod
+    def find_user_by_name(cls,first_name,second_name):
+
+        '''
+        Method that takes in a first_name and second_name and returns a user that matches that name.
+
+        Args:
+            first_name: first_name to search for
+            second_name: second_name to search for
+        Returns :
+            User that matches the name.
+        '''
+        for user in cls.user_list:
+            if user.name == name:
+                return user
+   
